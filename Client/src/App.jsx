d28328b3a2ade4d577f9c3b9e0c4f79d55e0a30a
Dilip Kumar <suthardilip189@gmail.com>
2024-08-21 12:54:29 +0530

    add Sponser.jsx

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Navbar from './components/Navbar';
 import Halloffame from './pages/Halloffame';
+import Teamview from './pages/Teamview';
+import Sponser from './pages/Sponser';
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/nav" element={<Navbar />} />
         <Route path="/halloffame" element={<Halloffame />} />
+        <Route path="/teamview" element={<Teamview />} />
+        <Route path="/sponser" element={<Sponser />} />
       </Routes>
     </Router>
   );
