1fcdad15827d5cdbbc1406c068c5a36dc81d3d74
Dilip Kumar <suthardilip189@gmail.com>
2024-08-28 19:14:51 +0530

    Create Connection

--- Server/app.js
+++ Server/app.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const connectDB = require("./config/db");
 const app = express();
 const PORT = 4000
+
+connectDB();
 
 app.get("/", (req, res) => {
     res.send("Hello");
