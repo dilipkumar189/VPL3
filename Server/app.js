dfa4a254093d1cb570e439760dfcfcc0fa8db408
Dilip Kumar <suthardilip189@gmail.com>
2024-08-29 14:50:43 +0530

    Create connection

--- Server/app.js
+++ Server/app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const connectDB = require("./config/db");
+require('dotenv').config();
 const app = express();
-const PORT = 4000
+const PORT = process.env.PORT
 
 connectDB();
 
--- Server/config/db.js
+++ Server/config/db.js
@@ -1,12 +1,12 @@
+// db.js
+
 const mongoose = require("mongoose");
 
 const connectDB = async() => {
+    console.log("MongoDB URI:", process.env.MONGODB_URI); // Debugging line
     try {
-        await mongoose.connect(process.env.MONGODB_URI, {
-          useNewUrlParser: true,
-          useUnifiedTopology: true,
-        });
-        console.log("Mongodb Connected Successfully");
+        await mongoose.connect(process.env.MONGODB_URI);
+        console.log("MongoDB Connected Successfully");
     }
     catch(error){
         console.log("MongoDB connection error", error);
