eed96e0dd1e1b3768a34c137a5df85b8060c8275
Dilip Kumar <suthardilip189@gmail.com>
2024-09-11 23:20:26 +0530

    Update route

--- Server/routes/route.js
+++ Server/routes/route.js
@@ -4,7 +4,7 @@ const upload = require('../middlewares/upload');
 const router = express.Router();
 // crate team 
 const { createTeam } = require('../controllers/addTeam');
-// const { addSptype } = require('../controllers/sponser');
+const { addSptype } = require('../controllers/sponser');
 // bhavesh 
 const { signUser, loginUser, getUser, editUserCaptain } = require('../controllers/userController');
 const { addSponsorType, getSponsorType, addFoodSpon, addOtherSpon, getFoodSpon, getOtherSpon } = require('../controllers/sponsorController');
