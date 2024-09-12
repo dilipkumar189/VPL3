b1064aad8fe9d38bb542653b307247c866a1fc88
Dilip Kumar <suthardilip189@gmail.com>
2024-09-12 17:10:07 +0530

    feat: Add ErrorBoundry Provider

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import {ErrorBoundary,ErrorFallback} from "./components"
 
-createRoot(document.getElementById('root')).render(
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ErrorBoundary FallbackComponent={ErrorFallback}>
+      <App />
+    </ErrorBoundary>
+  </StrictMode>
+);
