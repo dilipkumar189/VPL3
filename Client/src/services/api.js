eddb7f2b2e52e19663d289590a7a15f650662e16
Dilip Kumar <suthardilip189@gmail.com>
2024-09-12 11:43:39 +0530

    Update api's

--- Client/src/services/api.js
+++ Client/src/services/api.js
@@ -4,6 +4,74 @@ import axios from 'axios';
 const URL = 'http://localhost:4000';
 // const URL = 'http://74.208.226.181:5000';
 
+export const addFoodSP = async(data) => {
+  try {
+    return await axios.post(`${URL}/addFoodSpon`, data);
+  } catch (error) {
+    console.log("Error :- ", error);
+  }
+}
+
+
+export const getFoodSP = async () => {
+  try {
+    return await axios.get(`${URL}/getFoodSpon`);   
+  } catch (error) {
+    console.log("Error :- " , error);
+  }
+};
+
+export const addOtherSP = async(data) => {
+  try {
+    return await axios.post(`${URL}/addOtherSpon`, data);
+  } catch (error) {
+    console.log("Error :- ", error);
+  }
+}
+
+
+export const getOtherSP = async () => {
+  try {
+    return await axios.get(`${URL}/getOtherSpon`);   
+  } catch (error) {
+    console.log("Error :- " , error);
+  }
+};
+
+export const addAdvertise = async(data) => {
+  try {
+    return await axios.post(`${URL}/addAdvertise`, data);
+  } catch (error) {
+    console.log("Error :- ", error);
+  }
+}
+
+
+export const getAdvertiser = async () => {
+  try {
+    return await axios.get(`${URL}/getAdvertise`);   
+  } catch (error) {
+    console.log("Error :- " , error);
+  }
+};
+
+
+
+export const addSponsorType = async(data) => {
+  try {
+    return await axios.post(`${URL}/sponsorType`, data);
+  } catch (error) {
+    console.log("Error :- ", error);
+  }
+}
+
+export const getSponsorType = async () => {
+  try {
+    return await axios.get(`${URL}/getSponsorType`);   
+  } catch (error) {
+    console.log("Error :- " , error);
+  }
+};
 // dilip api ----------
 
 export const createTeam = async(data) => {
