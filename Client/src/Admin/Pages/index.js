85f4aaa966ac49eb7976f6dc105a41449756eef1
Dilip Kumar <suthardilip189@gmail.com>
2024-09-12 18:31:25 +0530

    refactor: Export pages from index

---
+++ Client/src/Admin/Pages/index.js
@@ -0,0 +1,29 @@
+import Dashboard from './Dashboard';
+import User from './User';
+import ATeam from './ATeam';
+import AAdvertiser from './AAdvertiser';
+import Advertiserform from './AAdvertiser/Advertiserform';
+import SType from './SType';
+import STypeform from './SType/STypeform';
+import FSponser from './ASponser/FSponser';
+import FSponserform from './ASponser/FSponserform';
+import OSponser from './ASponser/OSponser';
+import OSponserform from './ASponser/OSponserform';
+import AProfile from './AProfile';
+import AProfileform from './AProfile/AProfileform';
+  
+  export {
+    Dashboard,
+    User,
+    ATeam,
+    AAdvertiser,
+    Advertiserform,
+    SType,
+    STypeform,
+    FSponser,
+    FSponserform,
+    OSponser,
+    OSponserform,
+    AProfile,
+    AProfileform
+  };
---
+++ Client/src/User/pages/index.js
@@ -0,0 +1,42 @@
+import Home from './Home';
+// import Navbar from './Navbar';
+import HallOfFame from './HallOfFame';
+import TeamView from './TeamView';
+import Sponser from './Sponser';
+import Team from './Team';
+import Advertiser from './Advertiser';
+import Rules from './Rules';
+import AboutUs from './AboutUs';
+import ContactUs from './ContactUs';
+// import Profile from './Profile';
+// import Profileform from './Profileform';
+import Profile from './Profile';
+import EditProfile from './Profile/editProfile';
+import Teamform from './Teamform';
+import Teamform2 from './Teamform2';
+
+import Login from './login';
+import SignUp from './Signup';
+// import Dteamform from './Dteamform';
+
+
+export {
+    Home,
+    // Navbar,
+    HallOfFame,
+    TeamView,
+    Sponser,
+    Team,
+    Advertiser,
+    Rules,
+    AboutUs,
+    ContactUs,
+    Profile,
+    // Profileform,
+    EditProfile,
+    Teamform,
+    Teamform2,
+    Login,
+    SignUp,
+    // Dteamform
+    };
