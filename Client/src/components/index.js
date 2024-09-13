5644d345d0cc5c515a0e0ffc77c04482bb63a437
Dilip Kumar <suthardilip189@gmail.com>
2024-09-13 18:52:47 +0530

    Add readme

--- Client/src/components/index.js
+++
@@ -1,5 +0,0 @@
-import ErrorBoundary from "./ErrorBoundary";
-import ErrorFallback from "./ErrorFallback";
-import PageNotFound from "./PageNotFound";
-
-export { ErrorBoundary,ErrorFallback,PageNotFound}
--- Server/middlewares/upload.js
+++
@@ -1,9 +0,0 @@
-const multer = require('multer');
-
-// Configure multer for file handling
-const upload = multer({ 
-    storage: multer.diskStorage({}),
-    limits: {fileSize: 500000}
-}); // Temporary storage for uploaded files
-
-module.exports = upload
