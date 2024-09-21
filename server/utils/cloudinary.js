7b0abf4b1c304a5328bed513e6bd00942d4e40a4
Anand Suthar <anandsuthar956@gmail.com>
2024-09-21 18:27:13 +0530

    feat: ☁️ Improve file upload method and add delete functionality for Cloudinary

--- server/utils/cloudinary.js
+++ server/utils/cloudinary.js
@@ -1,22 +1,30 @@
-require('dotenv').config();
-const cloudinary = require('cloudinary').v2;
+require("dotenv").config();
+const cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME, 
-  api_key: process.env.API_KEY, 
-  api_secret: process.env.API_SECRET
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
 });
 
-const uploadFile = async (filePath) => {
+const uploadFile = async (file) => {
   try {
-      const result = await cloudinary.uploader.upload(filePath);
-      console.log(result);
-      return result;
+    return new Promise((resolve, reject) => {
+      const uploadStream = cloudinary.uploader.upload_stream(
+        { resource_type: "auto" },
+        (error, result) => {
+          if (error) reject(error);
+          else resolve(result.secure_url);
+        }
+      );
+
+      uploadStream.end(file.buffer);
+    });
   } catch (error) {
-      console.log(error.message);
-      throw error; 
+    console.error("Error uploading to Cloudinary:", error);
+    throw error;
   }
-}
+};
 
 const deleteFile = async (publicId) => {
   try {
@@ -27,9 +35,9 @@ const deleteFile = async (publicId) => {
     console.log(error.message);
     throw error;
   }
-}
+};
 
 module.exports = {
   uploadFile,
-  deleteFile
+  deleteFile,
 };
